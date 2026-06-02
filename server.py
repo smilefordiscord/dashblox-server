f1ea1764212985091db9ece0e25a4903e3339327
smile4 <bayoulionjr@gmail.com>
2026-06-02 12:54:07 -0800

    add db2 search and maxlvl

--- server.py
+++ server.py
@@ -3,12 +3,10 @@ import time
 from flask import Flask, request
 import json
 from json.encoder import JSONEncoder
-import os
 from flask import Flask, request
 import logging
-import warnings
-import time
-from datetime import datetime
+import os
+from psycopg2 import sql
 
 app = Flask(__name__)
 log = logging.getLogger('werkzeug')
@@ -92,191 +90,6 @@ def addlevel():
         logWarn(request.remote_addr, "GET", "/add-level", 403)
         return "Invalid method", 403
 
-@app.route('/cs-add-item', methods=['POST'])
-def csAddItem():
-    if request.method == 'POST':
-        cursor = conn.cursor()
-
-        data = request.get_json()
-        if data["secret"] != secret:
-            cursor.close()
-            return "Invalid secret", 403
-        
-        itemid = data["id"]
-        owner = data["owner"]
-        pattern = data["pattern"]
-        stattrak = data["st"]
-        wear = data["wear"]
-        cursor.execute("INSERT INTO public.rsitems (itemid, owner, pattern, stattrak, wear) VALUES (%(itemid)s, %(owner)s, %(pattern)s, %(stattrak)s, %(wear)s) RETURNING id;", {"itemid":itemid,"owner":owner,"pattern":pattern,"stattrak":stattrak,"wear":wear})
-        conn.commit()
-        
-        returnedLvls = JSONEncoder().encode(cursor.fetchone())
-        
-        cursor.close()
-        logInfo(request.remote_addr, "POST", "/cs-add-item", 200)
-        return returnedLvls, 200
-    else:
-        logWarn(request.remote_addr, "GET", "/cs-add-item", 403)
-        return "Invalid method", 403
-
-@app.route('/cs-add-items', methods=['POST'])
-def csAddItems():
-    if request.method == 'POST':
-        cursor = conn.cursor()
-
-        data = request.get_json()
-        if data["secret"] != secret:
-            cursor.close()
-            return "Invalid secret", 403
-        
-        returnedLvls = []
-        
-        amtOpened = 0
-
-        for item in data["items"]:
-            amtOpened += 1
-            itemid = item["id"]
-            owner = item["owner"]
-            pattern = item["pattern"]
-            stattrak = item["st"]
-            wear = item["wear"]
-            cursor.execute("INSERT INTO public.rsitems (itemid, owner, pattern, stattrak, wear) VALUES (%(itemid)s, %(owner)s, %(pattern)s, %(stattrak)s, %(wear)s) RETURNING id;", {"itemid":itemid,"owner":owner,"pattern":pattern,"stattrak":stattrak,"wear":wear})
-            conn.commit()
-            returnedLvls.append(cursor.fetchone()[0])
-        
-        cursor.execute("UPDATE public.rsplayerdata SET money = money + %(money)s WHERE userid = %(userid)s", {"money": data["cost"], "userid": data["userid"]})
-        cursor.execute("UPDATE public.rsplayerdata SET opened = opened + %(opened)s WHERE userid = %(userid)s", {"opened": amtOpened, "userid": data["userid"]})
-        conn.commit()
-        
-        cursor.close()
-        logInfo(request.remote_addr, "POST", "/cs-add-items", 200)
-        return returnedLvls, 200
-    else:
-        logWarn(request.remote_addr, "GET", "/cs-add-items", 403)
-        return "Invalid method", 403
-
-@app.route('/cs-delete-items', methods=['POST'])
-def csDeleteItems():
-    if request.method == 'POST':
-        cursor = conn.cursor()
-
-        data = request.get_json()
-        if data["secret"] != secret:
-            cursor.close()
-            return "Invalid secret", 403
-        
-        idList = ""
-        for id in data["ids"]:
-            if idList == "":
-                idList = str(id)
-            else:
-                idList = idList + ", " + str(id)
-        
-        idList = "DELETE FROM public.rsitems WHERE id IN (" + idList + ")"
-        cursor.execute(idList)
-        cursor.execute("UPDATE public.rsplayerdata SET money = money + %(money)s WHERE userid = %(userid)s", {"money": data["money"], "userid": data["userid"]})
-        conn.commit()
-
-        cursor.close()
-        logInfo(request.remote_addr, "POST", "/cs-delete-items", 200)
-        return "OK", 200
-    else:
-        logWarn(request.remote_addr, "GET", "/cs-delete-items", 403)
-        return "Invalid method", 403
-
-@app.route('/cs-trade-up', methods=['POST'])
-def csTradeUp():
-    if request.method == 'POST':
-        cursor = conn.cursor()
-
-        data = request.get_json()
-        if data["secret"] != secret:
-            cursor.close()
-            return "Invalid secret", 403
-        
-        idList = ""
-        for id in data["take"]:
-            if idList == "":
-                idList = str(id)
-            else:
-                idList = idList + ", " + str(id)
-        
-        idList = "DELETE FROM public.rsitems WHERE id IN (" + idList + ")"
-        cursor.execute(idList)
-        conn.commit()
-        
-        returnedLvls = []
-
-        for item in data["add"]:
-            itemid = item["id"]
-            owner = item["owner"]
-            pattern = item["pattern"]
-            stattrak = item["st"]
-            wear = item["wear"]
-            cursor.execute("INSERT INTO public.rsitems (itemid, owner, pattern, stattrak, wear) VALUES (%(itemid)s, %(owner)s, %(pattern)s, %(stattrak)s, %(wear)s) RETURNING id;", {"itemid":itemid,"owner":owner,"pattern":pattern,"stattrak":stattrak,"wear":wear})
-            conn.commit()
-            returnedLvls.append(cursor.fetchone()[0])
-        
-        cursor.close()
-        logInfo(request.remote_addr, "POST", "/cs-trade-up", 200)
-        return returnedLvls, 200
-    else:
-        logWarn(request.remote_addr, "GET", "/cs-trade-up", 403)
-        return "Invalid method", 403
-
-@app.route('/cs-get-inv', methods=['POST'])
-def csGetInv():
-    if request.method == 'POST':
-        cursor = conn.cursor()
-
-        data = request.get_json()
-        if data["secret"] != secret:
-            cursor.close()
-            return "Invalid secret", 403
-        
-        owner = data["owner"]
-        cursor.execute("SELECT * FROM public.rsitems WHERE owner = %(owner)s", {"owner":owner})
-        conn.commit()
-        
-        returnedLvls = JSONEncoder().encode(cursor.fetchall())
-        
-        cursor.close()
-        logInfo(request.remote_addr, "POST", "/cs-get-inv", 200)
-        return returnedLvls, 200
-    else:
-        logWarn(request.remote_addr, "GET", "/cs-get-inv", 403)
-        return "Invalid method", 403
-
-@app.route('/cs-leaderboard', methods=['POST'])
-def csLeaderboard():
-    if request.method == 'POST':
-        cursor = conn.cursor()
-
-        data = request.get_json()
-        if data["secret"] != secret:
-            cursor.close()
-            return "Invalid secret", 403
-        
-        cursor.execute("SELECT * FROM public.rsplayerdata ORDER BY money DESC LIMIT 100")
-        top100 = cursor.fetchall()
-        cursor.execute("SELECT * FROM public.rsplayerdata ORDER BY money ASC LIMIT 100")
-        bot100 = cursor.fetchall()
-        cursor.execute("SELECT * FROM public.rsplayerdata ORDER BY opened DESC LIMIT 100")
-        topUnboxers = cursor.fetchall()
-        
-        arr = [top100, bot100, topUnboxers]
-
-        returnedLvls = JSONEncoder().encode(arr)
-        
-        cursor.close()
-        logInfo(request.remote_addr, "POST", "/cs-leaderboard", 200)
-        return returnedLvls, 200
-    else:
-        logWarn(request.remote_addr, "GET", "/cs-leaderboard", 403)
-        return "Invalid method", 403
-
-@app.route('/cs-get-player-data', methods=['POST'])
-def csGetPlayerData():
     if request.method == 'POST':
         cursor = conn.cursor()
 
@@ -312,78 +125,257 @@ def csGetPlayerData():
         logWarn(request.remote_addr, "GET", "/cs-get-player-data", 403)
         return "Invalid method", 403
 
-# @app.route('/db2-get', methods=['POST'])
-# def db2get():
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         if data == None:
-#             return "Invalid", 403
-        
-#         if data["secret"] != secret:
-#             return "Invalid secret", 403
-        
-#         id = data["id"]
-        
-#         cursor = conn.cursor()
-#         try:
-#             cursor.execute("SELECT * FROM public.levels WHERE id = %(id)s", {'id':id})
-#             returnLevels =  JSONEncoder().encode(cursor.fetchone())
+@app.route('/db2-maxlvl', methods=['POST'])
+def db2get():
+    if request.method != 'POST':
+        return "Invalid method", 403
+    
+    data = request.get_json()
+    if data == None:
+        return "Invalid", 403
+    if data["secret"] != secret:
+        return "Invalid secret", 403
+    
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT * FROM public.levels ORDER BY id DESC LIMIT 1")
+        returnLevels =  JSONEncoder().encode(cursor.fetchone())
+        cursor.close()
+        return returnLevels, 200
+    except:
+        cursor.close()
+        return "Request failed", 404
 
-#             cursor.close()
-#             return returnLevels, 200
-#         except:
-#             cursor.close()
-#             return "Request failed", 404
-#     else:
-#         return "Invalid method", 403
+@app.route('/db2-search', methods=['POST'])
+def db2search():
+    if request.method != 'POST':
+        return "Invalid method", 403
+    
+    data = request.get_json()
+    if data is None:
+        return "Invalid", 403
+    if data.get("secret") != secret:
+        return "Invalid secret", 403
+    
+    searchText = f"%{data['searchText']}%"
+    sortType = data["sortType"]
+    max = data["max"]
 
-# @app.route('/db2-search', methods=['POST'])
-# def db2search():
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         if data == None:
-#             return "Invalid", 403
-#         if data["secret"] != secret:
-#             return "Invalid secret", 403
-        
+    cursor = conn.cursor()
+    try:
+        if max > 0:
+            query = sql.SQL("""
+                SELECT * FROM public.levels 
+                WHERE {0} < %s AND title ILIKE %s 
+                ORDER BY {0} DESC 
+                LIMIT 10
+            """).format(sql.Identifier(sortType))
+            
+            cursor.execute(query, (max, searchText))
+        else:
+            query = sql.SQL("""
+                SELECT * FROM public.levels 
+                WHERE title ILIKE %s 
+                ORDER BY {} DESC 
+                LIMIT 10
+            """).format(sql.Identifier(sortType))
+            
+            cursor.execute(query, (searchText,))
 
-
-#         cursor = conn.cursor()
-#         try:
-#             cursor.execute("SELECT * FROM public.levels WHERE id = %(id)s", {'id':id})
-#             returnLevels =  JSONEncoder().encode(cursor.fetchone())
-
-#             cursor.close()
-#             return returnLevels, 200
-#         except:
-#             cursor.close()
-#             return "Request failed", 404
-#     else:
-#         return "Invalid method", 403
-
-# @app.route('/db2-getSortedList', methods=['POST'])
-# def db2getSortedList():
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         if data["secret"] != secret:
-#             return "Invalid secret", 403
-        
-#         #TODO
-#         #sort option and limit option
-
-#         # cursor = conn.cursor()
-#         # try:
-#         #     cursor.execute("SELECT * FROM public.db2levels WHERE id = %(id)s", {'id':id})
-#         #     returnLevels =  JSONEncoder().encode(cursor.fetchall())
-
-#         #     cursor.close()
-#         #     return returnLevels, 200
-#         # except:
-#         #     cursor.close()
-#         #     return "Request failed", 404
-#     else:
-#         return "Invalid method", 403
+        returnLevels = JSONEncoder().encode(cursor.fetchall())
+        cursor.close()
+        print("DB2 Search: " + searchText)
+        return returnLevels, 200
+    except:
+        cursor.close()
+        return "Request failed", 404
 
 print("Starting server")
 
 app.run(host='0.0.0.0', port=8080)
+
+# @app.route('/cs-add-item', methods=['POST'])
+# def csAddItem():
+#     if request.method == 'POST':
+#         cursor = conn.cursor()
+
+#         data = request.get_json()
+#         if data["secret"] != secret:
+#             cursor.close()
+#             return "Invalid secret", 403
+        
+#         itemid = data["id"]
+#         owner = data["owner"]
+#         pattern = data["pattern"]
+#         stattrak = data["st"]
+#         wear = data["wear"]
+#         cursor.execute("INSERT INTO public.rsitems (itemid, owner, pattern, stattrak, wear) VALUES (%(itemid)s, %(owner)s, %(pattern)s, %(stattrak)s, %(wear)s) RETURNING id;", {"itemid":itemid,"owner":owner,"pattern":pattern,"stattrak":stattrak,"wear":wear})
+#         conn.commit()
+        
+#         returnedLvls = JSONEncoder().encode(cursor.fetchone())
+        
+#         cursor.close()
+#         logInfo(request.remote_addr, "POST", "/cs-add-item", 200)
+#         return returnedLvls, 200
+#     else:
+#         logWarn(request.remote_addr, "GET", "/cs-add-item", 403)
+#         return "Invalid method", 403
+
+# @app.route('/cs-add-items', methods=['POST'])
+# def csAddItems():
+#     if request.method == 'POST':
+#         cursor = conn.cursor()
+
+#         data = request.get_json()
+#         if data["secret"] != secret:
+#             cursor.close()
+#             return "Invalid secret", 403
+        
+#         returnedLvls = []
+        
+#         amtOpened = 0
+
+#         for item in data["items"]:
+#             amtOpened += 1
+#             itemid = item["id"]
+#             owner = item["owner"]
+#             pattern = item["pattern"]
+#             stattrak = item["st"]
+#             wear = item["wear"]
+#             cursor.execute("INSERT INTO public.rsitems (itemid, owner, pattern, stattrak, wear) VALUES (%(itemid)s, %(owner)s, %(pattern)s, %(stattrak)s, %(wear)s) RETURNING id;", {"itemid":itemid,"owner":owner,"pattern":pattern,"stattrak":stattrak,"wear":wear})
+#             conn.commit()
+#             returnedLvls.append(cursor.fetchone()[0])
+        
+#         cursor.execute("UPDATE public.rsplayerdata SET money = money + %(money)s WHERE userid = %(userid)s", {"money": data["cost"], "userid": data["userid"]})
+#         cursor.execute("UPDATE public.rsplayerdata SET opened = opened + %(opened)s WHERE userid = %(userid)s", {"opened": amtOpened, "userid": data["userid"]})
+#         conn.commit()
+        
+#         cursor.close()
+#         logInfo(request.remote_addr, "POST", "/cs-add-items", 200)
+#         return returnedLvls, 200
+#     else:
+#         logWarn(request.remote_addr, "GET", "/cs-add-items", 403)
+#         return "Invalid method", 403
+
+# @app.route('/cs-delete-items', methods=['POST'])
+# def csDeleteItems():
+#     if request.method == 'POST':
+#         cursor = conn.cursor()
+
+#         data = request.get_json()
+#         if data["secret"] != secret:
+#             cursor.close()
+#             return "Invalid secret", 403
+        
+#         idList = ""
+#         for id in data["ids"]:
+#             if idList == "":
+#                 idList = str(id)
+#             else:
+#                 idList = idList + ", " + str(id)
+        
+#         idList = "DELETE FROM public.rsitems WHERE id IN (" + idList + ")"
+#         cursor.execute(idList)
+#         cursor.execute("UPDATE public.rsplayerdata SET money = money + %(money)s WHERE userid = %(userid)s", {"money": data["money"], "userid": data["userid"]})
+#         conn.commit()
+
+#         cursor.close()
+#         logInfo(request.remote_addr, "POST", "/cs-delete-items", 200)
+#         return "OK", 200
+#     else:
+#         logWarn(request.remote_addr, "GET", "/cs-delete-items", 403)
+#         return "Invalid method", 403
+
+# @app.route('/cs-trade-up', methods=['POST'])
+# def csTradeUp():
+#     if request.method == 'POST':
+#         cursor = conn.cursor()
+
+#         data = request.get_json()
+#         if data["secret"] != secret:
+#             cursor.close()
+#             return "Invalid secret", 403
+        
+#         idList = ""
+#         for id in data["take"]:
+#             if idList == "":
+#                 idList = str(id)
+#             else:
+#                 idList = idList + ", " + str(id)
+        
+#         idList = "DELETE FROM public.rsitems WHERE id IN (" + idList + ")"
+#         cursor.execute(idList)
+#         conn.commit()
+        
+#         returnedLvls = []
+
+#         for item in data["add"]:
+#             itemid = item["id"]
+#             owner = item["owner"]
+#             pattern = item["pattern"]
+#             stattrak = item["st"]
+#             wear = item["wear"]
+#             cursor.execute("INSERT INTO public.rsitems (itemid, owner, pattern, stattrak, wear) VALUES (%(itemid)s, %(owner)s, %(pattern)s, %(stattrak)s, %(wear)s) RETURNING id;", {"itemid":itemid,"owner":owner,"pattern":pattern,"stattrak":stattrak,"wear":wear})
+#             conn.commit()
+#             returnedLvls.append(cursor.fetchone()[0])
+        
+#         cursor.close()
+#         logInfo(request.remote_addr, "POST", "/cs-trade-up", 200)
+#         return returnedLvls, 200
+#     else:
+#         logWarn(request.remote_addr, "GET", "/cs-trade-up", 403)
+#         return "Invalid method", 403
+
+# @app.route('/cs-get-inv', methods=['POST'])
+# def csGetInv():
+#     if request.method == 'POST':
+#         cursor = conn.cursor()
+
+#         data = request.get_json()
+#         if data["secret"] != secret:
+#             cursor.close()
+#             return "Invalid secret", 403
+        
+#         owner = data["owner"]
+#         cursor.execute("SELECT * FROM public.rsitems WHERE owner = %(owner)s", {"owner":owner})
+#         conn.commit()
+        
+#         returnedLvls = JSONEncoder().encode(cursor.fetchall())
+        
+#         cursor.close()
+#         logInfo(request.remote_addr, "POST", "/cs-get-inv", 200)
+#         return returnedLvls, 200
+#     else:
+#         logWarn(request.remote_addr, "GET", "/cs-get-inv", 403)
+#         return "Invalid method", 403
+
+# @app.route('/cs-leaderboard', methods=['POST'])
+# def csLeaderboard():
+#     if request.method == 'POST':
+#         cursor = conn.cursor()
+
+#         data = request.get_json()
+#         if data["secret"] != secret:
+#             cursor.close()
+#             return "Invalid secret", 403
+        
+#         cursor.execute("SELECT * FROM public.rsplayerdata ORDER BY money DESC LIMIT 100")
+#         top100 = cursor.fetchall()
+#         cursor.execute("SELECT * FROM public.rsplayerdata ORDER BY money ASC LIMIT 100")
+#         bot100 = cursor.fetchall()
+#         cursor.execute("SELECT * FROM public.rsplayerdata ORDER BY opened DESC LIMIT 100")
+#         topUnboxers = cursor.fetchall()
+        
+#         arr = [top100, bot100, topUnboxers]
+
+#         returnedLvls = JSONEncoder().encode(arr)
+        
+#         cursor.close()
+#         logInfo(request.remote_addr, "POST", "/cs-leaderboard", 200)
+#         return returnedLvls, 200
+#     else:
+#         logWarn(request.remote_addr, "GET", "/cs-leaderboard", 403)
+#         return "Invalid method", 403
+
+# @app.route('/cs-get-player-data', methods=['POST'])
+# def csGetPlayerData():
